05934571024904b5038f6f7f451be85980e9b735
JennyPersichini <nenny1096@gmail.com>
2023-04-14 15:36:26 +0200

    add bonus

---
+++ Bonus_CibiPreferiti/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/*
+ * BONUS
+ * Chiedere all'utente quanti cibi preferiti ha, e farglieli inserite da input della Console nell'array.
+ * Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.
+ */
+
+Console.Write("Quanti cibi preferiti hai? ");
+uint numeroCibi = uint.Parse(Console.ReadLine());
+
+string[] arrayCibi = new string[numeroCibi];
+
+for (int i = 0; i < numeroCibi; i++)
+{
+    Console.Write("Inserisci il cibo: ");
+    string food = Console.ReadLine();
+}
