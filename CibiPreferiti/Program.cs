8b73760c93f1a6b3467aff537917cc2dc61cf05a
JennyPersichini <nenny1096@gmail.com>
2023-04-14 14:36:05 +0200

    Aggiungere i file di progetto.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/* 
+ * Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi). 
+ * L’array deve essere già inizializzato nel programma ordinato già con i cibi dal più preferito al meno preferito, e i vostri cibi preferiti non vanno chiesti all’utente tramite input (hard-coded).
+ * Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+ * La lunghezza della classifica
+ * La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+ * Il vostro cibo top (prima posizione della classifica)
+ * Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+ */
+
+
+//inizializzo l'array
+
+string[] favoriteFoods = { "pizza", "frozen yogurt", "piadina", "bomba alla crema", "timballo", "broccoli", "polenta" };
+
+//stampo a video la lunghezza della classifica
+
+Console.WriteLine("Lunghezza: " + favoriteFoods.Length);
+
+//stampo l'intera classifica
+
+
+
+//stampo il cibo top
+
+
+
+//stampo il cibo preferito ma non troppo
